82afc66daf75da2a06acf07dd20c99f5d5340d81
nssamuels1 <95450798+nssamuels1@users.noreply.github.com>
2022-02-28 14:40:56 -0500

    Update index.js
    Lat. and Lon. changed to New York City

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 40.730610, lng: -73.935242 },
     zoom: 8,
   });
 }
